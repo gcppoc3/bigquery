68133464e4ba5c00238964ee61f4f85f0bb3c321
uzair <gcp.poc0111@gmail.com>
2026-06-10 13:01:38 +0000

    2 commit

---
+++ test_dataset/customer_master.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `prj-application-uat.test_dataset.customer_master`
+(
+    customer_id INT64,
+    customer_name STRING,
+    email STRING,
+    phone_number STRING,
+    city STRING,
+    state STRING,
+    country STRING,
+    created_timestamp TIMESTAMP
+);
