93f57282722a479d643d4d303e145b0fc31d992e
uzair <gcp.poc0111@gmail.com>
2026-06-10 12:30:10 +0000

    first commit

---
+++ test_dataset/employee_details.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `prj-application-uat.test_dataset.employee_details`
+(
+    employee_id INT64,
+    employee_name STRING,
+    department STRING,
+    salary NUMERIC,
+    joining_date DATE,
+    is_active BOOL,
+    created_timestamp TIMESTAMP
+);
